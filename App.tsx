898782e6181a431a2a111cafa85d592eff210e1e
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-22 22:25:18 -0300

    Configurando rotas

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { Login } from "./src/screens/Login";
+import NavigationRoutes from "@/routes";
 import "./src/styles/global.css";
 
 export default function App() {
-  return <Login />;
+  return <NavigationRoutes />;
 }
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export const Register = () => {
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text>Tela de registro</Text>
+        </View>
+    )
+}
