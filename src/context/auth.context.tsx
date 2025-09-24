564ec54c6e1f9e0f56a5c5f438788890d62828bc
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-23 23:14:08 -0300

    Integrando requisição no context

--- src/context/auth.context.tsx
+++ src/context/auth.context.tsx
@@ -1,31 +1,38 @@
-import { FormLoginParams } from '@/screens/Login/LoginForm'
-import { FormRegisterParams } from '@/screens/Register/RegisterForm'
+import { FormLoginParams } from "@/screens/Login/LoginForm";
+import { FormRegisterParams } from "@/screens/Register/RegisterForm";
 import {
-  createContext,
-  FC,
-  PropsWithChildren,
-  useContext,
-  useState,
-} from 'react'
-
+    createContext,
+    FC,
+    PropsWithChildren,
+    useContext,
+    useState,
+} from "react";
+import * as authService from "@/shared/services/dt-money/auth.service";
+import { IUser } from "@/shared/interfaces/user-interface";
 type AuthContextType = {
-  user: null
-  token: string | null
-  handleAuthenticate: (params: FormLoginParams) => Promise<void>
-  handleRegister: (params: FormRegisterParams) => Promise<void>
-  handleLogout: () => void
-}
+  user: IUser | null;
+  token: string | null;
+  handleAuthenticate: (params: FormLoginParams) => Promise<void>;
+  handleRegister: (params: FormRegisterParams) => Promise<void>;
+  handleLogout: () => void;
+};
 
-export const AuthContext = createContext<AuthContextType>({} as AuthContextType)
+export const AuthContext = createContext<AuthContextType>(
+  {} as AuthContextType
+);
 
 export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
-  const [user, setUser] = useState(null)
-  const [token, setToken] = useState(null)
+  const [user, setUser] = useState<IUser | null>(null);
+  const [token, setToken] = useState<string | null>(null);
 
-  const handleAuthenticate = async ({ email, password }: FormLoginParams) => {}
+  const handleAuthenticate = async (userData: FormLoginParams) => {
+    const { token, user } = await authService.authenticate(userData);
+    setUser(user);
+    setToken(token);
+  };
 
-  const handleRegister = async (formData: FormRegisterParams) => {}
-  const handleLogout = () => {}
+  const handleRegister = async (formData: FormRegisterParams) => {};
+  const handleLogout = () => {};
 
   return (
     <AuthContext.Provider
@@ -39,11 +46,11 @@ export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
     >
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
 export const useAuthContext = () => {
-  const context = useContext(AuthContext)
+  const context = useContext(AuthContext);
 
-  return context
-}
+  return context;
+};
