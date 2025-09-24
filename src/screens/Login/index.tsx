3b560f8dcea4dea7b3b496a91ab076fbb9d9b14d
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-23 22:27:58 -0300

    Criando hook useKeyboardVisible

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -3,6 +3,7 @@ import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { View } from "react-native";
 import { LoginForm } from "./LoginForm";
+import { AuthHeader } from "@/components/AuthHeader";
 
 export type PublicStackParamsList = {
   Login: undefined;
@@ -15,6 +16,7 @@ export const Login = () => {
   return (
     <DismissKeyboardView>
       <View className="flex-1 w-[82%] self-center">
+        <AuthHeader />
         <LoginForm />
       </View>
     </DismissKeyboardView>
