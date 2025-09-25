be9f452be1a1ebd0f85339d3b3850d9ed4db8123
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-24 22:17:58 -0300

    Consumindo dados do usuário do estado global

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,27 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { PublicRoutes } from './PublicRoutes'
-import { useCallback, useState } from 'react'
-import { PrivateRoutes } from './PrivateRoutes'
-import { SystemBars } from 'react-native-edge-to-edge'
+import { NavigationContainer } from "@react-navigation/native";
+import { PublicRoutes } from "./PublicRoutes";
+import { useCallback, useState } from "react";
+import { PrivateRoutes } from "./PrivateRoutes";
+import { SystemBars } from "react-native-edge-to-edge";
+import { useAuthContext } from "@/context/auth.context";
 
 const NavigationRoutes = () => {
-  const [user, setUser] = useState(undefined)
+  const { user, token } = useAuthContext();
 
   const Routes = useCallback(() => {
-    if (!user) {
-      return <PublicRoutes />
+    if (!user || !token) {
+      return <PublicRoutes />;
     } else {
-      return <PrivateRoutes />
+      return <PrivateRoutes />;
     }
-  }, [user])
+  }, [user, token]);
 
   return (
     <NavigationContainer>
       <SystemBars style="light" />
       <Routes />
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default NavigationRoutes
+export default NavigationRoutes;
