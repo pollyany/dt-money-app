7ad4fa50933e06243509b6dc9991c4b7a3e4dd61
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-27 20:54:43 -0300

    Buscando transações

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,29 +1,28 @@
-import { AppHeader } from "@/components/AppHeader";
-import { useAuthContext } from "@/context/auth.context";
-import { useTransactionContext } from "@/context/transaction.context";
-import { useErrorHandler } from "@/shared/hooks/useErrorHandler";
-import { useEffect } from "react";
-import { FlatList, Text, TouchableOpacity } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { ListHeader } from "./ListHeader";
+import { useTransactionContext } from '@/context/transaction.context'
+import { useErrorHandler } from '@/shared/hooks/useErrorHandler'
+import { useEffect } from 'react'
+import { FlatList } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { ListHeader } from './ListHeader'
 
 export const Home = () => {
-  const { fetchCategories } = useTransactionContext();
-  const { handleError } = useErrorHandler();
+  const { fetchCategories, fetchTransactions } = useTransactionContext()
+  const { handleError } = useErrorHandler()
 
   const handleFetchCategories = async () => {
     try {
-      await fetchCategories();
+      await fetchCategories()
     } catch (error) {
-      handleError(error);
+      handleError(error)
     }
-  };
+  }
 
   useEffect(() => {
-    (async () => {
-      await handleFetchCategories();
-    })();
-  }, []);
+    ;(async () => {
+      await handleFetchCategories()
+      await fetchTransactions()
+    })()
+  }, [])
 
   return (
     <SafeAreaView className="flex-1 bg-background-secondary">
@@ -33,5 +32,5 @@ export const Home = () => {
         renderItem={() => <></>}
       />
     </SafeAreaView>
-  );
-};
+  )
+}
--- src/shared/services/dt-money/transaction.service.ts
+++ src/shared/services/dt-money/transaction.service.ts
@@ -1,19 +1,38 @@
-import { dtMoneyApi } from '@/shared/api/dt-money'
-import { CreateTransactionInterface } from '@/shared/interfaces/https/create-transaction-request'
-import { TransactionCategory } from '@/shared/interfaces/https/transaction-category-response'
+import { dtMoneyApi } from "@/shared/api/dt-money";
+import { CreateTransactionInterface } from "@/shared/interfaces/https/create-transaction-request";
+import {
+  GetTransactionsParams,
+  GetTransactionsResponse,
+} from "@/shared/interfaces/https/get-transactions-request";
+import { TransactionCategory } from "@/shared/interfaces/https/transaction-category-response";
+import qs from "qs";
 
 export const getTransactionCategories = async (): Promise<
   TransactionCategory[]
 > => {
   const { data } = await dtMoneyApi.get<TransactionCategory[]>(
-    '/transaction/categories',
-  )
+    "/transaction/categories"
+  );
 
-  return data
-}
+  return data;
+};
 
 export const createTransaction = async (
-  transaction: CreateTransactionInterface,
+  transaction: CreateTransactionInterface
 ) => {
-  await dtMoneyApi.post('/transaction', transaction)
-}
+  await dtMoneyApi.post("/transaction", transaction);
+};
+
+export const getTransactions = async (
+  params: GetTransactionsParams
+): Promise<GetTransactionsResponse> => {
+  const { data } = await dtMoneyApi.get<GetTransactionsResponse>(
+    "/transaction",
+    {
+      params,
+      paramsSerializer: (p) => qs.stringify(p, { arrayFormat: "repeat" }),
+    }
+  );
+
+  return data;
+};
