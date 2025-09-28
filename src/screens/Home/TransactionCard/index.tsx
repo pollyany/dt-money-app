0bda9d59672d8c418323dc816dcda4ca0db1aff7
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-27 21:20:36 -0300

    Iniciando Modal de deleção

--- src/screens/Home/TransactionCard/index.tsx
+++ src/screens/Home/TransactionCard/index.tsx
@@ -1,39 +1,42 @@
-import { Transaction } from '@/shared/interfaces/transaction'
-import { FC } from 'react'
-import { Text, View } from 'react-native'
-import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable'
-import { MaterialIcons } from '@expo/vector-icons'
-import { format } from 'date-fns'
-import { colors } from '@/shared/colors'
-import { TransactionType } from '@/shared/enums/transaction-types'
-import clsx from 'clsx'
+import { Transaction } from "@/shared/interfaces/transaction";
+import { FC } from "react";
+import { Text, View } from "react-native";
+import Swipeable from "react-native-gesture-handler/ReanimatedSwipeable";
+import { MaterialIcons } from "@expo/vector-icons";
+import { format } from "date-fns";
+import { colors } from "@/shared/colors";
+import { TransactionType } from "@/shared/enums/transaction-types";
+import clsx from "clsx";
+import { RightAction } from "./RightAction";
 
 interface Params {
-  transaction: Transaction
+  transaction: Transaction;
 }
 
 export const TransactionCard: FC<Params> = ({ transaction }) => {
-  const isExpense = transaction.type.id === TransactionType.EXPENSE
+  const isExpense = transaction.type.id === TransactionType.EXPENSE;
 
   return (
     <Swipeable
       containerStyle={{
-        alignItems: 'center',
-        alignSelf: 'center',
-        overflow: 'visible',
-        width: '90%',
+        alignItems: "center",
+        alignSelf: "center",
+        overflow: "visible",
+        width: "90%",
         marginBottom: 16,
       }}
+      renderRightActions={() => <RightAction />}
+      overshootRight={false}
     >
       <View className="h-[140] bg-background-tertiary rounded-md p-6">
         <Text className="text-white text-base">{transaction.description}</Text>
         <Text
           className={clsx(
-            'text-xl font-bold mt-2',
-            isExpense ? 'text-accent-red' : 'text-accent-brand-light',
+            "text-xl font-bold mt-2",
+            isExpense ? "text-accent-red" : "text-accent-brand-light"
           )}
         >
-          {isExpense && '-'}R$ {transaction.value.toFixed(2).replace('.', ',')}
+          {isExpense && "-"}R$ {transaction.value.toFixed(2).replace(".", ",")}
         </Text>
         <View className="flex-row w-full justify-between items-center">
           <View className="items-center flex-row mt-3">
@@ -53,11 +56,11 @@ export const TransactionCard: FC<Params> = ({ transaction }) => {
               size={20}
             />
             <Text className="text-gray-700 text-base ml-2">
-              {format(transaction.createdAt, 'dd/MM/yyyy')}
+              {format(transaction.createdAt, "dd/MM/yyyy")}
             </Text>
           </View>
         </View>
       </View>
     </Swipeable>
-  )
-}
+  );
+};
