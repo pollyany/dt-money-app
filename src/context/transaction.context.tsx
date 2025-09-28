911badeca44bedd01c1d092ea90ffc82e425390f
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-27 21:03:52 -0300

    Exibindo última transação

--- src/context/transaction.context.tsx
+++ src/context/transaction.context.tsx
@@ -1,4 +1,4 @@
-import { TransactionCategory } from '@/shared/interfaces/https/transaction-category-response'
+import { TransactionCategory } from "@/shared/interfaces/https/transaction-category-response";
 import {
   createContext,
   FC,
@@ -6,53 +6,54 @@ import {
   useCallback,
   useContext,
   useState,
-} from 'react'
-import * as transactionService from '@/shared/services/dt-money/transaction.service'
-import { CreateTransactionInterface } from '@/shared/interfaces/https/create-transaction-request'
-import { TotalTransactions } from '@/shared/interfaces/https/total-transactions'
-import { Transaction } from '@/shared/interfaces/transaction'
+} from "react";
+import * as transactionService from "@/shared/services/dt-money/transaction.service";
+import { CreateTransactionInterface } from "@/shared/interfaces/https/create-transaction-request";
+import { TotalTransactions } from "@/shared/interfaces/https/total-transactions";
+import { Transaction } from "@/shared/interfaces/transaction";
 
 export type TransactionContextType = {
-  fetchCategories: () => Promise<void>
-  categories: TransactionCategory[]
-  createTransaction: (transaction: CreateTransactionInterface) => Promise<void>
-  fetchTransactions: () => Promise<void>
-  totalTransactions: TotalTransactions
-}
+  fetchCategories: () => Promise<void>;
+  categories: TransactionCategory[];
+  createTransaction: (transaction: CreateTransactionInterface) => Promise<void>;
+  fetchTransactions: () => Promise<void>;
+  totalTransactions: TotalTransactions;
+  transactions: Transaction[];
+};
 
-export const TransactionContext = createContext({} as TransactionContextType)
+export const TransactionContext = createContext({} as TransactionContextType);
 
 export const TransactionContextProvider: FC<PropsWithChildren> = ({
   children,
 }) => {
-  const [categories, setCategories] = useState<TransactionCategory[]>([])
-  const [transactions, setTransactions] = useState<Transaction[]>([])
+  const [categories, setCategories] = useState<TransactionCategory[]>([]);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [totalTransactions, setTotalTransactions] = useState<TotalTransactions>(
     {
       expense: 0,
       revenue: 0,
       total: 0,
-    },
-  )
+    }
+  );
 
   const fetchCategories = async () => {
     const categoriesResponse =
-      await transactionService.getTransactionCategories()
-    setCategories(categoriesResponse)
-  }
+      await transactionService.getTransactionCategories();
+    setCategories(categoriesResponse);
+  };
 
   const createTransaction = async (transaction: CreateTransactionInterface) => {
-    await transactionService.createTransaction(transaction)
-  }
+    await transactionService.createTransaction(transaction);
+  };
 
   const fetchTransactions = useCallback(async () => {
     const transactionsResponse = await transactionService.getTransactions({
       page: 1,
       perPage: 10,
-    })
-    setTransactions(transactionsResponse.data)
-    setTotalTransactions(transactionsResponse.totalTransactions)
-  }, [])
+    });
+    setTransactions(transactionsResponse.data);
+    setTotalTransactions(transactionsResponse.totalTransactions);
+  }, []);
 
   return (
     <TransactionContext.Provider
@@ -62,13 +63,14 @@ export const TransactionContextProvider: FC<PropsWithChildren> = ({
         createTransaction,
         fetchTransactions,
         totalTransactions,
+        transactions,
       }}
     >
       {children}
     </TransactionContext.Provider>
-  )
-}
+  );
+};
 
 export const useTransactionContext = () => {
-  return useContext(TransactionContext)
-}
+  return useContext(TransactionContext);
+};
